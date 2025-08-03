4f9c31ca13d01107d824f8709ba532b51f8d477b
araz0 <alhamdani.araz@gmail.com>
2025-08-03 15:12:49 +0200

    feat: Enhance HeroSection styling and add button for exploration

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -7,15 +7,30 @@ const StyledHeroSection = styled.section<{ imgSrc: string }>`
   background-position: center;
   height: 30vh;
   width: 100%;
-  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 
   h1 {
     color: white;
     font-size: 1.5rem;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+  }
+  button {
+    background-color: rgba(255, 255, 255, 0.8);
+    padding: 10px 20px;
+    border-radius: 5px;
+    font-size: 1rem;
+    color: black;
+    cursor: pointer;
+    border: none;
+    transition: background-color 0.3s;
+
+    &:hover {
+      background-color: rgba(255, 255, 255, 1);
+    }
   }
 `
 
@@ -26,6 +41,7 @@ export const HeroSectionRaw = ({ src }: HeroSectionProps) => {
   return (
     <StyledHeroSection imgSrc={src}>
       <h1>Hand Made Bags</h1>
+      <button>Explore all</button>
     </StyledHeroSection>
   )
 }
